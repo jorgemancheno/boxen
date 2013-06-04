e574e4194530f6d4921dd3ae91fe222b0628a3a2
Jorge Mancheno <jorge@pictela.com>
2013-06-04 18:36:46 -0400

    Updates.

--- modules/people/manifests/jorgemancheno.pp
+++ modules/people/manifests/jorgemancheno.pp
@@ -2,24 +2,20 @@ class people::jorgemancheno {
     include adium
     include alfred
     include better_touch_tools
+    include charles
     include chrome::dev
     include codekit
     include dropbox
     include firefox
+    include github_for_mac
     include gitx::dev
     include istatmenus4
     include iterm2::dev
-    include nodejs::v0_10
     include rdio
     include sequel_pro
     include transmit
+    include tunnelblick
     include vlc
 
-    # nodejs::module { ['coffee-script', 'meteorite']:
-    #    node_version => 'v0.10'
-    # }
-
-    class { 'nodejs::global':
-        version => 'v0.10'
-    }
+    # $home     = "/Users/${::boxen_user}"
 }
