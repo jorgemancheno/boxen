099a1942fb3478e7a6326e11ae15f2557d922337
Jorge Mancheno <git@jorgemancheno.com>
2013-06-05 00:09:53 -0400

    Adds dotfile installation and other stuff.

--- modules/people/manifests/jorgemancheno.pp
+++ modules/people/manifests/jorgemancheno.pp
@@ -1,21 +1,62 @@
 class people::jorgemancheno {
+    # Applications
     include adium
     include alfred
     include better_touch_tools
     include charles
     include chrome::dev
+    include chrome::canary
     include codekit
     include dropbox
     include firefox
     include github_for_mac
     include gitx::dev
+    include imageoptim
     include istatmenus4
     include iterm2::dev
     include rdio
     include sequel_pro
     include transmit
     include tunnelblick
+    include vagrant
+    include virtualbox
     include vlc
 
-    # $home     = "/Users/${::boxen_user}"
+    # Machine-specific apps
+    case $::hostname {
+        'ares': {
+            include handbrake
+            include onepassword
+            include transmission
+        }
+        default: {}
+    }
+
+    # Configuration Setup
+    $env = {
+        directories => {
+            home     => "/Users/${::boxen_user}",
+            repos    => "/Users/${::boxen_user}/Code",
+            dotfiles => "/Users/${::boxen_user}/Code/dotfiles"
+        }
+    }
+
+    # Dotfile Setup
+    file { "${env['directories']['repos']}":
+        ensure => "directory"
+    }
+    ~> repository { "${env['directories']['dotfiles']}":
+        source  => "jorgemancheno/dotfiles",
+        require => File["${env['directories']['repos']}"]
+    }
+    ~> exec { "Install dotfiles":
+        cwd         => "${env['directories']['dotfiles']}",
+        command     => "./sync.sh --force",
+        provider    => shell,
+        refreshonly => true,
+        require     => Repository["${env['directories']['dotfiles']}"]
+    }
+
+    # Set node.js version
+    class { 'nodejs::global': version => 'v0.10.5' }
 }
