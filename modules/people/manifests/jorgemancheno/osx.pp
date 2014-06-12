a3f1eb44f1acd967fc3e2825515aae23fb30ee87
Jorge Mancheno <git@jorgemancheno.com>
2014-06-12 18:49:05 -0400

    Disables showing of hidden files.

--- modules/people/manifests/jorgemancheno/osx.pp
+++ modules/people/manifests/jorgemancheno/osx.pp
@@ -46,7 +46,7 @@ class people::jorgemancheno::osx {
     include osx::finder::show_all_on_desktop # does all of the above
     include osx::finder::empty_trash_securely # enable Secure Empty Trash
     include osx::finder::unhide_library # unsets the hidden flag on ~/Library
-    include osx::finder::show_hidden_files
+    # include osx::finder::show_hidden_files
     include osx::finder::enable_quicklook_text_selection
 
     # == Universal Access Settings
